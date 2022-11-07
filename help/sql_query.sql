027eae4cab901f2cf33c54379e80b81436837f40
renameira <renatomeira91@gmail.com>
2022-11-07 10:33:53 -0300

    Add sql query

---
+++ help/sql_query.sql
@@ -0,0 +1,28 @@
+show databases;
+
+create database vsearchlogDB;
+
+use vsearchlogDB;
+
+show tables;
+
+grant all on vsearchlogdb.* to 'vsearch' identified by 'password';
+
+create table log(
+id int auto_increment primary key,
+ts timestamp default current_timestamp,
+phrase varchar(128) not null,
+letters varchar(32) not null,
+ip varchar(16) not null,
+browser_string varchar(256) not null,
+results varchar(64) not null);
+
+
+select * from log;
+
+select 
+	id 
+from
+	log
+where ip = '127.0.0.1'
+order by ts desc;
