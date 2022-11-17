86f7850422b738cbf1d81141d2fe20a68da3a106
renameira <renatomeira91@gmail.com>
2022-11-16 21:44:24 -0300

    Remove some files

--- webapp/src/DBcm.py
+++ webapp/src/DBcm.py
@@ -1,16 +1,43 @@
 import mysql.connector
 
 
+class ConnectionError(Exception):
+    """Raised if the backend-database cannot be connected to."""
+
+    pass
+
+
+class CredentialsError(Exception):
+    """Raised if the database is up, but there's a login issue."""
+
+    pass
+
+
+class SQLError(Exception):
+    """Raised if the query caused problems."""
+
+    pass
+
+
 class UseDatabase:
     def __init__(self, configuration: dict):
         self.configuration = configuration
 
-    def __enter__(self) -> "cursor":
-        self.conn = mysql.connector.connect(**self.configuration)
-        self.cursor = self.conn.cursor()
-        return self.cursor
+    def __enter__(self):
+        try:
+            self.conn = mysql.connector.connect(**self.configuration)
+            self.cursor = self.conn.cursor()
+            return self.cursor
+        except mysql.connector.errors.InterfaceError as err:
+            raise ConnectionError(err) from err
+        except mysql.connector.errors.ProgrammingError as err:
+            raise CredentialsError(err) from err
 
     def __exit__(self, exc_type, exc_value, exc_trace):
         self.conn.commit()
         self.cursor.close()
         self.conn.close()
+        if exc_type is mysql.connector.errors.ProgrammingError:
+            raise SQLError(exc_value)
+        elif exc_type:
+            raise exc_type(exc_value)
---
+++ webapp/src/decorator.py
@@ -0,0 +1,21 @@
+from flask import session, render_template
+from functools import wraps
+
+
+def check_logged_in(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if "logged_in" in session:
+            return func(*args, **kwargs)
+        title = "You are NOT logged in."
+        return render_template("message.html", the_title=title)
+
+    return wrapper
+
+
+def uppercase_output(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return func().upper()
+
+    return wrapper
